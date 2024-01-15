3eced1bccdc225e4f349c7fd48751764e86462de
makkenzo <nekgo2009@gmail.com>
2024-01-15 14:46:12 +0600

    Refactor NotePreviewList component and add resetScroll functionality
    This commit refactors the NotePreviewList component to accept an optional onSelect prop. It also adds the resetScroll functionality to the App component, which is triggered when a note is selected in the NotePreviewList. These changes improve the user experience by resetting the scroll position when a new note is selected.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,17 +8,27 @@ import {
     MarkdownEditor,
     FloatingNoteTitle,
 } from '@/components'
+import { ElementRef, useRef } from 'react'
 
 const App = () => {
+    const contentContainerRef = useRef<ElementRef<'div'>>(null)
+
+    const resetScroll = () => {
+        contentContainerRef.current?.scrollTo(0, 0)
+    }
+
     return (
         <>
             <DraggableTopBar />
             <RootLayout>
                 <SideBar className="p-2">
                     <ActionButtonsRow className="flex justify-between mt-1" />
-                    <NotePreviewList className="mt-3 space-y-1" />
+                    <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
                 </SideBar>
-                <Content className="border-l bg-zinc-900/50 border-l-white/20">
+                <Content
+                    ref={contentContainerRef}
+                    className="border-l bg-zinc-900/50 border-l-white/20"
+                >
                     <FloatingNoteTitle className="pt-2" />
                     <MarkdownEditor />
                 </Content>
