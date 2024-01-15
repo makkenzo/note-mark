d7b19f49c00a78e731bf3968db07923e10ac21b1
makkenzo <nekgo2009@gmail.com>
2024-01-15 11:22:38 +0600

    Update code formatting and remove unused code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit/eslint-config-ts/recommended',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        '@electron-toolkit/eslint-config-ts/recommended',
+        '@electron-toolkit/eslint-config-prettier',
+    ],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+    },
 }
